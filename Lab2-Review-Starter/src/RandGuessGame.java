dccc3433cf8b31790f622fcc4656d4e31613905a
Cameron <Cameron@Cameron>
2019-01-17 20:18:15 -0600

    1.17.19

---
+++ Lab2-Review-Starter/src/RandGuessGame.java
@@ -0,0 +1,89 @@
+// COURSE: CSCI1620
+// TERM: Spring 2019
+//
+// NAME: Cameron Gilinsky
+// RESOURCES:
+
+import java.util.Random;
+
+/**
+ * Class for a simple, randomized guessing game. Five integer values between 1 and MAX_VALUE (inclusive) will be
+ * generated. Only the first and last will be shown to the player. The player must then guess if the sum
+ * of all of the numbers is greater than the possible average or not.
+ * @author Cameron Gilinsky
+ */
+public class RandGuessGame
+{
+
+	//Declare data members
+	private final int MAX_VALUE = 100;
+	private final int ARRAY_LENGTH = 5;
+	public int[] arr = new int[5];
+	public char answer;
+	public int sum;
+	
+	Random rand = new Random();
+	
+	//Create Constructor
+	public RandGuessGame(Random inner) 
+	{
+		rand = inner;
+		
+		for (int k = 0; k < ARRAY_LENGTH; k++)
+		{
+			arr[k] = rand.nextInt(MAX_VALUE);
+		}
+	}
+	
+	//Write member methods
+	
+	/**
+	 * Retrieves the numbers array. Used for testing, do not change.
+	 * @return The numbers array.
+	 */
+	public int[] getNumbers()
+	{
+		
+		return numbers;
+	}
+	
+	/**
+	 * Retrieves the sum of the numbers in the array. Used for testing, do not change.
+	 * @return The value of arraySum.
+	 */
+	public int getArraySum()
+	{
+		int arraySum = 0;
+		for (int j = 0; j < 5; j++)
+		{
+			arraySum += arr[j];
+		}
+		sum = arraySum;
+		return arraySum;
+	}
+	
+	public int populateArray()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			arr[i] = rand.nextInt(100);
+		}
+		return 0;
+	}
+	
+	public boolean validatePlayerGuess(char yesOrNo)
+	{
+		answer = yesOrNo;
+		return (yesOrNo == 'Y' || yesOrNo == 'N');
+	}
+	
+	public String getResult()
+	{
+		String result = new String("");
+		
+		if (answer == 'Y' && sum > 250)
+			result = "You guessed correctly!";
+		
+		return result;
+	}
+}
